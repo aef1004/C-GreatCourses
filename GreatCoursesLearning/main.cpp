064f56b0a874105d8f5e402fd707b65f0aacf491
Amy Fox <aef1004@gmail.com>
2020-02-24 21:44:11 -0700

    Initial Commit

---
+++ GreatCoursesLearning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GreatCoursesLearning
+//
+//  Created by AmyFox on 2/24/20.
+//  Copyright © 2020 AmyFox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
